6e153826f2d00080800911bc596be9e5c771f915
Umair Abdus Salam <umairabdsalam@gmail.com>
2024-04-10 02:59:41 +0100

    Initial Commit

---
+++ Trekr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trekr
+//
+//  Created by Umair Salam on 4/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Umair Salam on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
